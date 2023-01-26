79d43a56f052a2410a02715cb92f08a54eb45cbe
myselfshravan <shravanrevanna@gmail.com>
2023-01-26 20:28:52 +0530

    changed footer 4

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -24,9 +24,7 @@ import logoAirbnb from '@/images/logos/airbnb.svg'
 import logoFacebook from '@/images/logos/facebook.svg'
 import logoPlanetaria from '@/images/logos/planetaria.svg'
 import logoStarbucks from '@/images/logos/starbucks.svg'
-import { generateRssFeed } from '@/lib/generateRssFeed'
-import { getAllArticles } from '@/lib/getAllArticles'
-import { formatDate } from '@/lib/formatDate'
+
 
 function MailIcon(props) {
   return (
